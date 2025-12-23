73ecc53cdd3bd3b7801c25098e5eeee3ee648df8
wuaadi <wu.aadi@gmail.com>
2025-12-22 22:02:32 -0800

    Update testingcode.ino

--- testingcode.ino
+++ testingcode.ino
@@ -10,7 +10,6 @@ test code for LAZULUM--test different functions with serial, breadboard, etc.
 
 #ifndef sbi
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#define F_CPU  16000000 //timer clock runs at 16MHz
 
 #endif //not necessrary for synth
 
@@ -24,9 +23,11 @@ main loop:       update envelopes & voice arr, handle serial i/o
 */
 /*GLOBAL CONSTANTS
 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+const uint32_t CPU_RATE = 16000000; //timer clock runs at 16MHz
 const int NUMVOICES = 4; const int DEADNAME = -1;
 const int bits_phase_accumulator = 32;
-const int SAMPLE_RATE = 20000; //20k Hz sample rate
+const uint16_t SAMPLE_RATE = 16000; //20k Hz sample rate
+
 const int WAVETABLE_SIZE = 256;
 int prescaler_timer1 = 1; //hardcoded for now, determined by TCCR1B
 const uint16_t ISR_MARGIN = 50; //measured in ticks
@@ -38,7 +39,6 @@ uint8_t potA, potD, potR; //potentiometer values
 volatile unsigned long samplecnt = 0;
 volatile bool voicemixflag, envupdateflag, midicompleteflag, printflag, LEDflag, safeguardflag = false;
 uint16_t output = 0; //0-255
-
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 //MIDI globals
 volatile uint8_t MIDIstatus, MIDInote, MIDIvelo, MIDIbyte_index;
@@ -47,15 +47,21 @@ typedef enum {IDLE, ATTACK, DECAY, SUSTAIN, RELEASE} EnvelopeStage;
 typedef enum {SINE, SQUARE, SAW, TRIANGLE, MOOG, SHARK, FORMANT, MS20, NUM_OSC_TYPES} OscillatorType;
 
 typedef struct {
-  bool pressed; int name; uint16_t amp; uint8_t raw; uint8_t env_amp;
-  EnvelopeStage stage; OscillatorType osc; 
-  uint32_t phase; uint32_t phase_inc; unsigned long startcnt;
-  uint16_t envIndex; float f; //frequency
-  uint8_t a_inc, d_inc, r_inc; //attack,decay,release increments
+  volatile bool pressed;
+  volatile uint8_t env_amp;
+  volatile uint8_t raw;
+  volatile EnvelopeStage stage;
+  volatile uint16_t envIndex;
+  volatile uint16_t amp;
+  volatile uint32_t phase;
+  volatile uint32_t phase_inc;
+  int name;
+  unsigned long startcnt;
+  float f;
+  uint8_t a_inc, d_inc, r_inc;
   uint8_t amp_before_r;
-  uint8_t * wavetable;
+  const uint8_t * wavetable;
 } Voice;
-
 typedef struct {
   volatile bool active; volatile uint8_t name;
 } Input;
@@ -205,17 +211,73 @@ const uint8_t ms20table[256] PROGMEM = {
 
 
 //asdr tables
-uint8_t Atable[WAVETABLE_SIZE];
-uint8_t Dtable[WAVETABLE_SIZE];
-uint8_t Rtable[WAVETABLE_SIZE];
+const uint8_t Atable[WAVETABLE_SIZE] = {
+  0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+  16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,
+  32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,
+  48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,
+  64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,
+  80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,
+  96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,
+  112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,
+  128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,
+  144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,
+  160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,
+  176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,
+  192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,
+  208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,
+  224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,
+  240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255
+};
 
-const float MIDIfreqtable[129] PROGMEM = {8.18,8.66,9.18,9.72,10.30,10.91,11.56,12.25,12.98,13.75,14.57,15.43,16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 
-23.12, 24.50, 25.96, 27.50, 29.14, 30.87, 32.70, 34.65, 36.71, 38.89, 41.20, 43.65, 46.25, 49.00, 51.91, 55.00, 58.27, 61.74, 65.41, 69.30, 73.42, 77.78, 
-82.41, 87.31, 92.50, 98.00, 103.83, 110.00, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94, 261.63, 
-277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99, 
-830.61, 880.00, 932.33, 987.77, 1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.00, 1864.66, 1975.53, 2093.00, 2217.46, 2349.32, 
-2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520.00, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040.00, 
-7458.62, 7902.13, 8372.02, 8869.84, 9397.27, 9956.06, 10548.08, 11175.30, 11839.82, 12543.85, 13289.75};
+const uint8_t Dtable[WAVETABLE_SIZE] = {
+  255,254,253,252,251,250,249,248,247,246,245,244,243,242,241,240,
+  239,238,237,236,235,234,233,232,231,230,229,228,227,226,225,224,
+  223,222,221,220,219,218,217,216,215,214,213,212,211,210,209,208,
+  207,206,205,204,203,202,201,200,199,198,197,196,195,194,193,192,
+  191,190,189,188,187,186,185,184,183,182,181,180,179,178,177,176,
+  175,174,173,172,171,170,169,168,167,166,165,164,163,162,161,160,
+  159,158,157,156,155,154,153,152,151,150,149,148,147,146,145,144,
+  143,142,141,140,139,138,137,136,135,134,133,132,131,130,129,128,
+  127,126,125,124,123,122,121,120,119,118,117,116,115,114,113,112,
+  111,110,109,108,107,106,105,104,103,102,
+  102,102,102,102,102,102,102,102,102,102,102,102,102,102,102,102,
+  102,102,102,102,102,102,102,102,102,102,102,102,102,102,102,102,
+  102,102,102,102,102,102,102,102,102,102,102,102,102,102,102,102,
+  102,102,102,102,102,102,102,102,102,102,102,102,102,102,102,102,
+  102,102,102,102,102,102,102,102,102,102,102,102,102,102,102,102,
+  102,102,102,102,102,102,102,102,102,102
+};
+
+const uint8_t Rtable[WAVETABLE_SIZE] = {
+  255,254,253,252,251,250,249,248,247,246,245,244,243,242,241,240,
+  239,238,237,236,235,234,233,232,231,230,229,228,227,226,225,224,
+  223,222,221,220,219,218,217,216,215,214,213,212,211,210,209,208,
+  207,206,205,204,203,202,201,200,199,198,197,196,195,194,193,192,
+  191,190,189,188,187,186,185,184,183,182,181,180,179,178,177,176,
+  175,174,173,172,171,170,169,168,167,166,165,164,163,162,161,160,
+  159,158,157,156,155,154,153,152,151,150,149,148,147,146,145,144,
+  143,142,141,140,139,138,137,136,135,134,133,132,131,130,129,128,
+  127,126,125,124,123,122,121,120,119,118,117,116,115,114,113,112,
+  111,110,109,108,107,106,105,104,103,102,101,100,99,98,97,96,
+  95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,
+  79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,
+  63,62,61,60,59,58,57,56,55,54,53,52,51,50,49,48,
+  47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,
+  31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,
+  15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0
+};
+
+
+const float MIDIfreqtable[129] PROGMEM = {
+  8.18,8.66,9.18,9.72,10.30,10.91,11.56,12.25,12.98,13.75,14.57,15.43,16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 
+  23.12, 24.50, 25.96, 27.50, 29.14, 30.87, 32.70, 34.65, 36.71, 38.89, 41.20, 43.65, 46.25, 49.00, 51.91, 55.00, 58.27, 61.74, 65.41, 69.30, 73.42, 77.78, 
+  82.41, 87.31, 92.50, 98.00, 103.83, 110.00, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94, 261.63, 
+  277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99, 
+  830.61, 880.00, 932.33, 987.77, 1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.00, 1864.66, 1975.53, 2093.00, 2217.46, 2349.32, 
+  2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520.00, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040.00, 
+  7458.62, 7902.13, 8372.02, 8869.84, 9397.27, 9956.06, 10548.08, 11175.30, 11839.82, 12543.85, 13289.75
+};
 
 /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 
@@ -238,13 +300,14 @@ void incrementADSR_OLD(Voice* v);
 
 void setup() {
   //PWM and GATE outputs
+  OCR0A = 0;
+  OCR0B = 0;
+  TCCR0A &= ~(_BV(COM0A1) | _BV(COM0B1)); // disconnect PWM outputs
   pinMode(11, OUTPUT); //mixed output
   pinMode(3, OUTPUT); pinMode(5, OUTPUT); pinMode(6, OUTPUT); pinMode(9, OUTPUT); //individual voices
   pinMode(A1, INPUT); pinMode(A2, INPUT); pinMode(A3, INPUT); //pot inputs adsr
   pinMode(13, INPUT_PULLUP); //button to toggle oscillator mode
   pinMode(7, OUTPUT); //error button for voices going out of bounds
-  digitalWrite(7, LOW);
-  BLINK(7, 100, 3);
   osc_button_press_cnt = 0;
   //MIDI setup
   // Set baud rate to 31,250
@@ -260,6 +323,7 @@ void setup() {
 
   //timer setup
   cli();
+
   TCCR1A = 0; // Normal operation 
   TCCR1B = 0; TCNT1 = 0; 
   TCCR1B |= (1 << WGM12); // CTC mode 
@@ -274,10 +338,8 @@ void setup() {
   //Serial.begin(115200);
   // initialize voices:
   init_voicearray(edgar);
-
-  float lambda_decay = 0.02f;
-  float lambda_release = 0.01f;
-  float sustain = 0.5;
+  //float lambda_decay = 0.02f;
+  //float lambda_release = 0.01f;
   sei();
 } //setup
 
@@ -302,20 +364,33 @@ void loop() {
     if (MIDIstatus == 0x90 && MIDIvelo > 0) {
       //find off voices first
       potA = analogRead(A1); potD = analogRead(A2); potR = analogRead(A3); 
-      unsigned long oldest_start = UINT32_MAX; int i_oldest = 0; bool foundoff = false;
+      unsigned long oldest_start = UINT32_MAX; int i_oldest = 0; bool useoldest = true; bool useidle = true;
       for (int i=0; i<NUMVOICES; i++) {
-          if (edgar[i].stage == IDLE) {key_press(osctype, samplecnt, midiNoteToFreq(MIDInote), &edgar[i], SAMPLE_RATE, MIDInote); foundoff=true; break;}
-          if (edgar[i].startcnt < oldest_start) {oldest_start = edgar[i].startcnt; i_oldest = i;}
-          //velocity not handled for now
+        Voice *v = &edgar[i];
+        if (v->name == MIDInote) {
+          useoldest = false; useidle = false; 
+          key_press(osctype, samplecnt, midiNoteToFreq(MIDInote), v, SAMPLE_RATE, MIDInote);
+          break;
+        }
+      }
+      if (useidle){
+        for (int i=0; i<NUMVOICES; i++) {
+          Voice *v = &edgar[i];
+            if (v->name == MIDInote) {useoldest = false; break;}
+            if (v->stage == IDLE) {key_press(osctype, samplecnt, midiNoteToFreq(MIDInote), v, SAMPLE_RATE, MIDInote); useoldest=false; break;}
+            if (v->startcnt < oldest_start) {oldest_start = v->startcnt; i_oldest = i;}
+            //velocity not handled for now
+        }
       }
       //if none, find oldest voice to replace
-      if (foundoff==false) {key_press(osctype, samplecnt, midiNoteToFreq(MIDInote), &edgar[i_oldest], SAMPLE_RATE, MIDInote);}
+      if (useoldest) {key_press(osctype, samplecnt, midiNoteToFreq(MIDInote), &edgar[i_oldest], SAMPLE_RATE, MIDInote);}
     }
     //CASE: Note-off
     if (MIDIstatus == 0x80 || (MIDIstatus == 0x90 && MIDIvelo == 0)){
       for (int i=0; i<NUMVOICES; i++) {
-        if (edgar[i].name == MIDInote)
-          {key_release(&edgar[i]); break;}
+        Voice * v = &edgar[i];
+        if (v->name == MIDInote && v->pressed)
+          {key_release(v); break;}
       }
     }
     //DEBUG: check for ZOMBIE voices
@@ -329,7 +404,7 @@ void loop() {
         if (v1->name == v2->name) digitalWrite(7, HIGH);
       }
     }
-  */
+    */
   midicompleteflag = false;
   } //midicompleteflag
 
@@ -406,8 +481,7 @@ void loop() {
         osc_button_press_cnt = 0;
       osctype = (OscillatorType) osc_button_press_cnt;
     }
-    if (safeguardflag) digitalWrite(7, HIGH); else digitalWrite(7, LOW);
-    safeguardflag = false;
+    if (safeguardflag) {PIND = (1 << 7); safeguardflag = false;}
     LEDflag = false;
   }
 
@@ -439,33 +513,35 @@ ISR(TIMER1_COMPA_vect) {
   if (TCNT1 > SAFE_LIMIT) {safeguardflag = true; return;}
   samplecnt++;
   //WAVETABLE GENERATION /
-  output = 0;
+  uint16_t current_output = 0;
   for (int i=0; i<NUMVOICES; i++) {
     Voice * v = &edgar[i];
-    if (v->env_amp == 0) continue;
+    uint8_t temp_env_amp = v->env_amp;
+    if (temp_env_amp == 0) continue;
     // PHASE ACCUMULATION
-    v->phase += v->phase_inc;
+    uint32_t temp_phase = v->phase;
+    uint32_t temp_phase_inc = v->phase_inc;
+    temp_phase += temp_phase_inc;
+    v->phase = temp_phase;
     //WAVETABLE--just raw oscillation
     //v->raw = pgm_read_byte(&sinetable[edgar[i].phase >> 24]);
-    v->raw = pgm_read_byte(v->wavetable[v->phase >> 24]);
+    uint8_t idx = v->phase >> 24;
+    uint8_t raw_sample = pgm_read_byte(v->wavetable + idx);
+    v->raw = raw_sample;
     //WAVETABLE--envelope-scaled oscillation
-    v->amp = (uint16_t)edgar[i].raw * (uint16_t)edgar[i].env_amp >> 8; //
-    output += v->amp;
+    uint16_t temp_amp = (uint16_t)raw_sample * (uint16_t)temp_env_amp;
+    v->amp = temp_amp >> 8;
+    current_output += v->amp;
   }//for
   
   //VOICE MIXING
-  output >>= NUMVOICES; // Normalize mixed output
-  OCR2A = output;
+  OCR2A = (uint8_t)(current_output >> 2); //divide by NUMVOICES = 4
   
   //VOICE HANDLING DONE BY MIDI ISR
 
   //Update ENVELOPE
-  if ((samplecnt & 39) == 0) envupdateflag = true; //run every 40 samples
-
-  //LED
-  if ((samplecnt & 99) == 0) LEDflag = true;
-  //printing
-  if ((samplecnt & 59999) == 0) printflag = true;
+  if ((samplecnt & 31) == 0) envupdateflag = true; //run every 32 samples
+  if ((samplecnt & 63) == 0) LEDflag = true; //run every 64 samples
 } //ISR
 
 /*ISR for MIDI
@@ -473,7 +549,7 @@ ISR(TIMER1_COMPA_vect) {
 
 ISR(USART_RX_vect) {
   if (UCSR0A & _BV(DOR0)) { //in case of data overrun, reset MIDI input
-    digitalWrite(7, HIGH);
+    //digitalWrite(7, HIGH);
   }
   uint8_t byte = UDR0;
   //3 bytes: status, data (note and velocity)
@@ -518,25 +594,30 @@ void generate_env_tables(EnvelopeStage stage, float lambda, float sustain, uint8
 
 void init_voicearray(Voice * edgar) {
   for (int i=0; i<NUMVOICES; i++) {
-    edgar[i].pressed=false; edgar[i].amp=0; edgar[i].stage=IDLE; edgar[i].osc=SINE; 
+    edgar[i].pressed=false; edgar[i].amp=0; edgar[i].stage=IDLE; 
     edgar[i].name = DEADNAME; edgar[i].phase = 0; edgar[i].phase_inc= 0; edgar[i].envIndex = 0; edgar[i].f = 20; // 20 hz
+    edgar[i].env_amp=0; edgar[i].wavetable = sinetable;
   } 
 }//init_voicearray
 
 void key_release(Voice * v) {
-  v->pressed = false;
+  cli(); v->pressed = false; sei();
 }
 
 void key_off(Voice * v) {
-  v->stage = IDLE; v->name = DEADNAME; v->env_amp=0;
+  cli(); v->stage = IDLE; v->name = DEADNAME; v->env_amp=0; sei();
 }
 
 void key_press(OscillatorType osc, unsigned long startcnt, float freq, Voice* v, int SAMPLE_RATE, int name) {
-  v->startcnt = startcnt; v->f = freq; v->stage = ATTACK; v->osc = osc;
-  cli(); v->envIndex = 0; sei();
+  cli();
+  v->startcnt = startcnt; 
+  v->f = freq; 
+  v->stage = ATTACK;
+  v->envIndex = 0;
   v->name = name;
   v->phase_inc = (unsigned long) ((float)v->f * (unsigned long) (1 << bits_phase_accumulator) / SAMPLE_RATE);
-  v->phase = 0; v-> pressed = true;
+  v->phase = 0; 
+  v-> pressed = true;
   v->a_inc = convertADSR(potA); v->d_inc = convertADSR(potD); v->r_inc = convertADSR(potR);
   switch (osc) {
         case SINE: v->wavetable = sinetable; break;
@@ -546,7 +627,8 @@ void key_press(OscillatorType osc, unsigned long startcnt, float freq, Voice* v,
         case SHARK: v->wavetable = sharktable; break;
         case FORMANT: v->wavetable = formanttable; break;
         case MS20: v->wavetable = ms20table; break;
-      } 
+  }
+  sei(); 
 }
 
 uint8_t convertADSR(uint8_t pot) {return 1 + (pot * (WAVETABLE_SIZE - 1)) / 1023;}
@@ -613,11 +695,11 @@ void incrementADSR(Voice* v) {
     case RELEASE:
       v->envIndex += v->r_inc;
       if (v->envIndex >= WAVETABLE_SIZE) {key_off(v);}
-      v->env_amp = (Rtable[v->envIndex] * v->amp_before_r) >> 8; 
+      else {v->env_amp = (Rtable[v->envIndex] * v->amp_before_r) >> 8;}
       break;
   }
 } //incrementADSR_NEW
 
 float midiNoteToFreq(uint8_t note) {
-	return pgm_read_byte(&MIDIfreqtable[note]);
+	return pgm_read_float(&MIDIfreqtable[note]);
 }
